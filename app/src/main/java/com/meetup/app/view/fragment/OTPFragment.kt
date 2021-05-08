5c60585a4ec390d47511d9da4bae6c2162f68bf5
viroth-geek <tyviroth.planb@gmail.com>
2021-05-08 20:47:58 +0700

    Design dialog

--- app/src/main/java/com/meetup/app/view/fragment/OTPFragment.kt
+++ app/src/main/java/com/meetup/app/view/fragment/OTPFragment.kt
@@ -1,12 +1,19 @@
 package com.meetup.app.view.fragment
 
-import androidx.lifecycle.ViewModelProvider
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 import com.meetup.app.R
+import com.meetup.app.databinding.FragmentOtpBinding
 
 class OTPFragment : Fragment() {
 
@@ -14,19 +21,57 @@ class OTPFragment : Fragment() {
         fun newInstance() = OTPFragment()
     }
 
-    private lateinit var viewModel: OTPViewModel
+    private val viewModel: OTPViewModel by viewModels()
+    private lateinit var binding: FragmentOtpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_otp, container, false)
+        binding = FragmentOtpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OTPViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        binding.phoneNumberTextField.editText?.doOnTextChanged { text, start, before, count ->
+
+        }
+
+        binding.button.setOnClickListener {
+            if (binding.phoneNumberTextField.editText?.text!!.contains("i love you")) {
+                showDialog(requireContext().getColor(R.color.blue), title = "Trov hz",description = "jng ban kuy oy slanh hehe 😍😍")
+
+            } else {
+                showDialog(requireContext().getColor(R.color.red), title = "Error Hz",description = "Check mdong tt. khos ey hz")
+            }
+        }
+    }
+
+    private fun showDialog(color: Int, title: String, description: String) {
+
+        val builder = AlertDialog.Builder(requireContext(), R.style.MeetupWidgetDialog)
+        val inflater = requireActivity().layoutInflater
+        val view = inflater.inflate(R.layout.component_dialog, null)
+
+//        view.setBackgroundColor(
+//            ContextCompat.getColor(
+//                requireContext(),
+//                android.R.color.transparent
+//            )
+//        )
+
+        builder.setView(view)
+        val alertDialog = builder.create()
+        alertDialog.show()
+        view.findViewById<MaterialButton>(R.id.button).setOnClickListener {
+            alertDialog.dismiss()
+        }
+        view.findViewById<MaterialTextView>(R.id.titleTextView).text = title
+        view.findViewById<MaterialTextView>(R.id.titleTextView).setTextColor(color)
+        view.findViewById<MaterialTextView>(R.id.descriptionTextView).text = description
+
     }
 
 }
